1ec89658393b9e5bf338c8385eaaf71e48fb7cc5
George Kirby <georgeskirby@btinternet.com>
2019-07-30 14:17:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,9 +3,16 @@ def prompt_user
   puts "Guess a number between 1 and 6"
 end
 
+def get_user_input
+  gets.chomp
+end
 
 
 def run_guessing_game
   computers_number = rand(6) + 1
+  prompt_user
+  user_input = get_user_input
   
+  if computers_number == user_input
+    puts "You guessed the correct number!"
 end
