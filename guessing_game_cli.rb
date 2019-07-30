525fa49063f1d376ab4ef4e32c9d2236ba8544c3
George Kirby <georgeskirby@btinternet.com>
2019-07-30 14:20:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,12 +7,16 @@ def get_user_input
   gets.chomp
 end
 
-
 def run_guessing_game
   computers_number = rand(6) + 1
   prompt_user
   user_input = get_user_input
   
-  if computers_number == user_input
+  if user_input == "exit"
+    puts "Goodbye!"
+  elsif user_input == computers_number
     puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{computers_number}."
+  end
 end
